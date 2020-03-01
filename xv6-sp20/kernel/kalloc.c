04f3faa5268e45508a9ef43eb39d36682c3e3b12
Zhengyi Chen <zchen597@wisc.edu>
2020-02-29 18:16:39 -0600

    dump

--- xv6-sp20/kernel/kalloc.c
+++ xv6-sp20/kernel/kalloc.c
@@ -17,6 +17,15 @@ struct {
   struct run *freelist;
 } kmem;
 
+
+typedef struct allolist{
+    struct allolist* next;  //a ptr to the head of an allocated list
+    int addr;
+}
+int allo_sz; //size of the allocated list
+allolist* head;
+allolist* new;
+
 extern char end[]; // first address after kernel loaded from ELF file
 
 // Initialize free list of physical pages.
@@ -27,8 +36,14 @@ kinit(void)
 
   initlock(&kmem.lock, "kmem");
   p = (char*)PGROUNDUP((uint)end);
-  for(; p + PGSIZE <= (char*)PHYSTOP; p += PGSIZE)
-    kfree(p);
+  for(; p + PGSIZE <= (char*)PHYSTOP; p += PGSIZE){
+    kfree(p);}
+
+  head->addr  = NULL;
+  head-> next = NULL;
+  allo_sz = 0 ;
+
+
 }
 
 // Free the page of physical memory pointed at by v,
@@ -39,8 +54,9 @@ void
 kfree(char *v)
 {
   struct run *r;
+  allolist * head_st = head;//head store
 
-  if((uint)v % PGSIZE || v < end || (uint)v >= PHYSTOP) 
+  if((uint)v % PGSIZE || v < end || (uint)v >= PHYSTOP)
     panic("kfree");
 
   // Fill with junk to catch dangling refs.
@@ -50,7 +66,26 @@ kfree(char *v)
   r = (struct run*)v;
   r->next = kmem.freelist;
   kmem.freelist = r;
-  release(&kmem.lock);
+  //remove r from allocated list
+  if(head->addr == (int)r){
+      head = head ->next;
+      allo_sz --;
+      goto release;
+  }
+
+  while(head -> next != NULL){
+      if(head -> next->addr == (int)r){
+          head -> next = head -> next -> next;
+          allo_sz --;
+          goto restore;
+      }
+      head = head -> next;
+
+  }
+  restore:
+    head = head_st;
+  release:
+    release(&kmem.lock);
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -61,11 +96,36 @@ kalloc(void)
 {
   struct run *r;
 
+
   acquire(&kmem.lock);
   r = kmem.freelist;
   if(r)
     kmem.freelist = r->next;
+    if(allo_sz != 0){
+        new-> addr = (int) r;
+        head -> next = head;
+        head = new;
+    }else{
+        head-> addr = (int) r;
+    }
+    allo_sz ++;
   release(&kmem.lock);
+
   return (char*)r;
 }
 
+//This function return an array of most recently allocated pages with size numframes
+//The array will be filled to *frames
+//Return -1 if numframes > total allocated pages
+int dump_allocated(int *frames, int numframes) {
+    if( numframes > allo_sz) return -1;
+
+    allolist * curr = head;
+    for( int i=0; i< numframes; i++){
+        *(frames+i) = curr->addr;
+        curr= curr -> next;
+    }
+
+
+    return 0 ;
+}
