c16f04d34d414cf94bb69820724d439ced654e79
Zhengyi Chen <zchen597@wisc.edu>
2020-03-02 20:27:17 -0600

    check r->next

--- xv6-sp20/kernel/kalloc.c
+++ xv6-sp20/kernel/kalloc.c
@@ -29,7 +29,7 @@ void
 kinit(void)
 {
   char *p;
-  
+
 
   initlock(&kmem.lock, "kmem");
   p = (char*)PGROUNDUP((uint)end);
@@ -87,6 +87,7 @@ kalloc(void)
   acquire(&kmem.lock);
   r = kmem.freelist;
   if(r){
+      if(r->next == NULL){return 0; }
     kmem.freelist = r->next->next;
     for(int i = allo_sz-1; i >= 0; i--){
       allolist[i+1] = allolist[i];
